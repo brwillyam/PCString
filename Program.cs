41bb76f9a8a2417ca0cacaf77be0bd446a39db14
Willyam D <br.willyam@hotmail.com>
2021-02-09 14:57:23 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace Pstring2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "Teste de inversao de frase sem o uso do reverse";
+            StringBuilder reversao = new StringBuilder();
+            int length = frase.Length -1;
+            while(length >= 0)
+            {
+                 reversao.Append(frase[length]);
+                 length--;
+            }
+            Console.WriteLine($"String original: {frase}");
+            Console.WriteLine($"String invertida {reversao}");
+
+            for(int i= frase.Length -1; i >= 0; i--)
+            {
+                reversao.Append(frase[i]);
+            }
+            Console.WriteLine($"String invertida {reversao}");
+
+        }
+    }
+}
